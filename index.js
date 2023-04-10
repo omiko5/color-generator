f415905e04dfe3038bc7f1dad485b4458feebb69
omiko <omiko.sherozia@gmial.com>
2023-04-10 22:40:32 +0400

    first part

---
+++ index.js
@@ -0,0 +1,58 @@
+const hexInput = document.getElementById('hexInput');
+const inputColor = document.getElementById('inputColor');
+const sliderText = document.getElementById('sliderText');
+const slider = document.getElementById('slider');
+
+hexInput.addEventListener('keyup', () => {
+  
+  const hex = hexInput.value;
+  if(!isValidHex(hex)) return;
+  
+  const strippedHex = hex.replace('#', '');
+
+  inputColor.style.backgroundColor = "#" + strippedHex;  
+})
+
+const isValidHex = (hex) => {
+    if(!hex) return false;
+    
+    const strippedHex = hex.replace('#', '');
+    return strippedHex.length === 3 || strippedHex.length === 6;
+}
+
+const convertHexToRGB = (hex) => {
+  if(!isValidHex(hex)) return null;
+  
+  let strippedHex = hex.replace('#', '');
+
+  if(strippedHex.length === 3){
+    strippedHex = strippedHex[0] + strippedHex[0] 
+    + strippedHex[1] + strippedHex[1] 
+    + strippedHex[2] + strippedHex[2];
+  }
+  
+  const r  = parseInt(strippedHex.substring(0,2), 16);
+  const g  = parseInt(strippedHex.substring(2,4), 16);
+  const b  = parseInt(strippedHex.substring(4,6), 16);
+  
+  return {r,g,b};
+}
+
+const convertRGBToHex = (r,g,b) => {
+  const firstPair = ("0" + r.toString(16)).slice(-2);
+  const secondPair = ("0" + g.toString(16)).slice(-2);
+  const thirdPair = ("0" + b.toString(16)).slice(-2);
+  
+  const hex = "#" + firstPair + secondPair + thirdPair;
+  return hex;
+}
+
+//Create the alterColor function which accepts hex value and percentage
+//convert the hex value to rgb
+//increase each r,g,b value by appropriate amount (percentage of 255)
+//use the new r,g,b values to convert to a hex value
+//return the hex value
+
+slider.addEventListener('input', () => {
+  sliderText.textContent = `${slider.value}%`;
+})
